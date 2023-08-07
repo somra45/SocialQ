737521b9daa97433d0fb53643d28cc0ec002b805
root <somra45@gmail.com>
2023-08-07 10:54:07 -0400

    start users

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+router.post('/register', async (req, res, next) => {
+  // Check to make sure no one has already registered with the proposed email or
+  // username.
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  });
+
+  if (user) {
+    // Throw a 400 error if the email address and/or username already exists
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === req.body.username) {
+      errors.username = "A user has already registered with this username";
+    }
+    err.errors = errors;
+    return next(err);
+  }
 });
 
 module.exports = router;
