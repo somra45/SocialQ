50cd7825ac6d45d28cdcbbccbecec1494b793182
root <somra45@gmail.com>
2023-08-07 11:21:50 -0400

    user auth working

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 const csurf = require('csurf');
 const debug = require('debug');
 require('./models/User');
+require('./config/passport');
+const passport = require('passport');
 
 const cors = require('cors');
 const { isProduction } = require('./config/keys');
@@ -19,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser()); 
+app.use(passport.initialize());
 
 if (!isProduction) {
     app.use(cors());
