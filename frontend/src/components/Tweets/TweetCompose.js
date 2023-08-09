f90464652f5944ece35f82ad57c81d8f6092dd30
mxcrpntr <carpenter.max@gmail.com>
2023-08-09 12:03:03 -0400

    first ai commit

--- frontend/src/components/Tweets/TweetCompose.js
+++ frontend/src/components/Tweets/TweetCompose.js
@@ -4,6 +4,7 @@ import { clearTweetErrors, composeTweet } from '../../store/tweets';
 import TweetBox from './TweetBox';
 import './TweetCompose.css';
 import NavBar from '../NavBar/NavBar';
+import jwtFetch from '../../store/jwt';
 
 function TweetCompose () {
   const [body, setBody] = useState('');
@@ -11,18 +12,59 @@ function TweetCompose () {
   const author = useSelector(state => state.session.user);
   const newTweet = useSelector(state => state.tweets.new);
   const errors = useSelector(state => state.errors.tweets);
+  const [userInstructions,setUserInstructions] = useState("");
+  // const [aiInstructions,setAiInstructions] = useState("");
+  const [generatedBody,setGeneratedBody] = useState("");
+  const [categoryArray,setCategoryArray] = useState([]);
+  const [mediaDescArray,setMediaDescArray] = useState([]);
+  const [triggerGeneration,setTriggerGeneration] = useState(false);
+
 
   useEffect(() => {
     return () => dispatch(clearTweetErrors());
   }, [dispatch]);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch(composeTweet({ body })); 
-    setBody('');
-  };
+  // const handleGenerate = e => {
+  //   e.preventDefault();
+  //   dispatch(composeTweet({ body })); 
+  //   setBody('');
+  // };
 
-  const update = e => setBody(e.currentTarget.value);
+  useEffect(() => {
+    console.log(triggerGeneration)
+    console.log(userInstructions)
+    if (triggerGeneration && userInstructions) {
+      const fetchGeneration = async (instructions) => {
+        const res = await jwtFetch(`/api/aiFetch`, {
+          method: "POST",
+          body: instructions
+        })
+        if (res.ok) {
+          setGeneratedBody(res)
+        }
+      }
+      let categoryString = ""
+      let mediaDescString = ""
+      if (categoryArray.length > 0) {
+        categoryString = categoryArray.join(", ")
+      }
+      if (mediaDescArray.length > 0) {
+        mediaDescString = mediaDescArray.join(", ")
+      }
+      const aiInstructions =
+        `Please respond to this with a tweet written according to the following instructions: ${userInstructions}.
+        ${categoryString ? `Also, please write the tweet so that its tone and content lines up with all of the following categories: ${categoryString}` : ""}
+        ${mediaDescString ? `Also, keep in mind that photos/videos with the following descriptions will be attached to the tweet: ${mediaDescString}` : ""}`
+      debugger
+      fetchGeneration(aiInstructions);
+      // dispatch(composeTweet({body: aiInstructions}))
+    }
+    if (triggerGeneration) setTriggerGeneration(false);
+  }, [triggerGeneration])
+
+  const update = (e) => {
+    setUserInstructions(e.currentTarget.value);
+  }
 
   return (
     <>
@@ -31,25 +73,34 @@ function TweetCompose () {
 
         <div className='compose-tweet-area'>
 
-          <form className="compose-tweet" onSubmit={handleSubmit}>
             <textarea
               rows="9"
               cols="20"
               wrap='soft'
               className='compose-tweet-input'
               type="textarea"
-              value={body}
+              value={userInstructions}
               onChange={update}
-              placeholder="Give us a breif description of how you would like your Tweet to sound"
+              placeholder="Give us a brief description of how you would like your Tweet to sound"
               required
             />
+            {generatedBody && (
+              <textarea
+              rows="4"
+              cols="20"
+              wrap='soft'
+              className='generated-tweet-body'
+              type="textarea"
+              value={generatedBody}
+              required
+              />
+            )}
 
             <div className="errors">{errors?.text}</div>
 
             {/* UNDER THIS IS WHERE THE TAGS FORM WILL GO  */}
 
-            <div>
-              <form className="tag-area" onSubmit={handleSubmit}>
+            {/* <div>
                 <input 
                   type="checkbox"
                   value=""
@@ -71,21 +122,14 @@ function TweetCompose () {
                 required
                 /> 
 
-                {/* this is where the extra tags will be */}
-       
-                 {/* this is where the extra tags will be */}
-
                 <div className="errors">{errors?.text}</div>
 
 
-              </form>
-                <input className="addTagsButton"type="submit" value="Add Tags" />
-            </div>
+            </div> */}
             
             {/* BEFORE THIS IS WHERE TAGS WILL GO */}
-            <input  className="generateButton"type="submit" value="GENERATE" />
+            <button  className="generateButton" onClick={() => {setTriggerGeneration(true)}}>{ generatedBody ? "Regenerate" : "Generate"}</button>
 
-          </form>
         </div>
 
         {/* <div className="tweet-preview">
