4e7ff9f2d0d4a647dbe3fa2917f197ea8a50063f
Joe Randazzo <jprandazzo@icloud.com>
2023-08-09 12:04:54 -0400

    build seed tweets

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
 const Tweet = require('../models/Tweet');
+const Category = require('../models/Category');
+const PostCategory = require('../models/PostCategory');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker'); 
+const Category = require("../models/Category.js");
 
 const NUM_SEED_USERS = 10;
 const NUM_SEED_TWEETS = 30;
@@ -121,12 +124,50 @@ tweets.push(
     EVER‼️‼️‼️`,
     author: User.findOne({firstName: 'Cher', lastName: 'Sarkissian'})._id,
     date: new Date('June 30, 2023, 4:51 PM EDT'),
+    photoUrl1: 'https://pbs.twimg.com/media/F2JSzA7WYAAMkPQ?format=jpg&name=small',
+    photoDesc1: "bus with cher's image on the side that says Cherlato on it",
+    photoUrl2: 'https://pbs.twimg.com/media/F2JSzAzXQAEQ1AF?format=jpg&name=360x360',
+    photoDesc2: 'gelato with colorful toppings in a cone',
+    photoUrl3: 'https://pbs.twimg.com/media/F2JSzA7XAAUujYF?format=jpg&name=360x360',
+    photoDesc3: 'chocolate gelato with crunchy topping in a cone',
     replyCount: 364,
     retweetCount: 607,
     quoteTweetCount: 193,
     likeCount: 4578,
     viewCount: 326000,
     bookmarkCount: 30,
+    createdOnSocialQ: false
+  }),
+  new Tweet ({
+    body: `Available Now:bangbang: Cher’s It’s a Man’s World (Deluxe Edition) - This Limited-Edition LP Box Contains The Remastered Record & Rare Remixes – Both For The First Time On Red, Blue, Green & Yellow Vinyl Plus Two Exclusive, Numbered Lithographs
+    http://lnk.to/itsamansworld
+    🍎🐍✨`,
+    author: User.findOne({firstName: 'Cher', lastName: 'Sarkissian'})._id,
+    date: new Date('July 14, 2023, 2:40 PM EDT'),
+    replyCount: 145,
+    retweetCount: 241,
+    quoteTweetCount: 54,
+    likeCount: 1506,
+    viewCount: 116300,
+    bookmarkCount: 16,
+    videoUrl1: 'https://twitter.com/i/status/1679894982533033992', // need to edit
+    videoDesc1: `flipping through pages of Cher's book`,
+    createdOnSocialQ: false
+  }),
+  new Tweet ({
+    body: `Listen To “Paradise Is Here (Sunrise Mix)” From The Forthcoming Deluxe Edition of ‘It’s a Man’s World.’ Available For The First Time On Vinyl 7/14 :bangbang:
+    https://lnk.to/pihsm
+    🩷`,
+    author: User.findOne({firstName: 'Cher', lastName: 'Sarkissian'})._id,
+    date: new Date('July 12, 2023, 4:40 PM EDT'),
+    replyCount: 120,
+    retweetCount: 199,
+    quoteTweetCount: 28,
+    likeCount: 1578,
+    viewCount: 105500,
+    bookmarkCount: 19,
+    videoUrl1: 'https://twitter.com/i/status/1679894982533033992', // need to edit
+    videoDesc1: `cover of cher's single Paradise is Here while track plays`,
     createdOnSocialQ: false
   }),
   new Tweet ({
@@ -162,6 +203,34 @@ tweets.push(
     likeCount: 4095,
     viewCount: 893000,
     bookmarkCount: 45,
+    createdOnSocialQ: false
+  }),
+  new Tweet ({
+    body: `Doing Vinyl 2`,
+    author: User.findOne({firstName: 'Cher', lastName: 'Sarkissian'})._id,
+    date: new Date('June 30, 2023, 8:24 PM EDT'),
+    replyCount: 199,
+    retweetCount: 123,
+    quoteTweetCount: 25,
+    likeCount: 1751,
+    viewCount: 126400,
+    bookmarkCount: 7,
+    createdOnSocialQ: false
+  }),
+  new Tweet ({
+    body: `Listen to “One by One (JR’s Pride Mix)” From The Forthcoming Deluxe Edition of ‘It’s a Man’s World.’ Available For The First Time On Vinyl From 7/14 - https://lnk.to/cobo
+    🔥
+    #OneByOne #DeluxeEdition #VinylRelease #MusicMagic #WarnerRecords`,
+    author: User.findOne({firstName: 'Cher', lastName: 'Sarkissian'})._id,
+    date: new Date('June 29, 2023, 8:24 PM EDT'),
+    replyCount: 148,
+    retweetCount: 197,
+    quoteTweetCount: 15,
+    likeCount: 1416,
+    viewCount: 105300,
+    bookmarkCount: 9,
+    videoUrl1: '', // need to edit
+    videoDesc1: `still of Cher's One by One single while track plays in background`,
     createdOnSocialQ: false
   }),
 
@@ -228,6 +297,83 @@ tweets.push(
     bookmarkCount: 9,
     createdOnSocialQ: false
   }),
+  new Tweet ({
+    body: `What do you think, bestie? 🤔`,
+    author: User.findOne({firstName: 'Cookie', lastName: 'Monster'})._id,
+    date: new Date('June 29, 2023, 1:12 PM EDT'),
+    replyCount: 23,
+    retweetCount: 267,
+    quoteTweetCount: 10,
+    likeCount: 2357,
+    viewCount: 112000,
+    bookmarkCount: 9,
+    videoUrl1: '', //need to edit
+    videoDesc1: `cookie monster asking: 'if cookie crumbles in the forest, and me not there to eat it, is it still delicious?'`,
+    createdOnSocialQ: false
+  }),
+  new Tweet({
+    body: `Me just tried to bake cookies, but me got distracted by the chocolate chips. Oops! Me think me invented a new cookie invention. 🍪🍫 #BakingAdventure`,
+    author: User.findOne({ firstName: 'Cookie', lastName: 'Monster' })._id,
+    date: new Date(),
+    replyCount: 42,
+    retweetCount: 103,
+    quoteTweetCount: 5,
+    likeCount: 1589,
+    viewCount: 78500,
+    bookmarkCount: 11,
+    createdOnSocialQ: true
+  }),
+  
+  new Tweet({
+    body: `Sometimes me wonder, why is it called a "computer" and not a "cookie-ter"? Me could definitely use more cookies in technology! 💻🍪 #TechThoughts`,
+    author: User.findOne({ firstName: 'Cookie', lastName: 'Monster' })._id,
+    date: new Date(),
+    replyCount: 31,
+    retweetCount: 78,
+    quoteTweetCount: 2,
+    likeCount: 1876,
+    viewCount: 92000,
+    bookmarkCount: 7,
+    createdOnSocialQ: true
+  }),
+  
+  new Tweet({
+    body: `Me just discovered a hidden stash of cookies! Me always knew me had a secret cookie treasure. 🏴‍☠️🍪 #CookieAdventure`,
+    author: User.findOne({ firstName: 'Cookie', lastName: 'Monster' })._id,
+    date: new Date(),
+    replyCount: 15,
+    retweetCount: 55,
+    quoteTweetCount: 3,
+    likeCount: 1234,
+    viewCount: 64000,
+    bookmarkCount: 6,
+    createdOnSocialQ: true
+  }),
+  
+  new Tweet({
+    body: `Me have a cookie strategy: eat one cookie, then eat another cookie, and then eat more cookies! Me call it "Strategic Cookie Consumption." 🍪🍪🍪 #CookieLife`,
+    author: User.findOne({ firstName: 'Cookie', lastName: 'Monster' })._id,
+    date: new Date(),
+    replyCount: 20,
+    retweetCount: 87,
+    quoteTweetCount: 6,
+    likeCount: 1450,
+    viewCount: 76000,
+    bookmarkCount: 8,
+    createdOnSocialQ: true
+  }),
+  new Tweet({
+    body: `Today me trying to set world record for most cookies eaten in one minute. Me reached three cookies before me fell asleep. Me call it "Nap Time Records." 😴🍪 #CookieChampion`,
+    author: User.findOne({ firstName: 'Cookie', lastName: 'Monster' })._id,
+    date: new Date(),
+    replyCount: 12,
+    retweetCount: 42,
+    quoteTweetCount: 1,
+    likeCount: 1012,
+    viewCount: 58000,
+    bookmarkCount: 5,
+    createdOnSocialQ: true
+  }),
 
   //Lord Voldemort tweets
   new Tweet ({
@@ -284,7 +430,66 @@ tweets.push(
     quoteTweetCount: 1,
     likeCount: 1719,
     createdOnSocialQ: false
-  })
+  }),
+  new Tweet({
+    body: `Plans for world domination? Completed it, mate.`,
+    author: User.findOne({ firstName: 'Lord', lastName: 'Voldemort' })._id,
+    date: new Date('October 10, 2022, 9:45 AM EDT'),
+    replyCount: 29,
+    retweetCount: 850,
+    quoteTweetCount: 37,
+    likeCount: 3286,
+    bookmarkCount: 1,
+    createdOnSocialQ: false
+  }),
+  
+  new Tweet({
+    body: `Just had a lovely stroll through the Forbidden Forest. Met a couple of Acromantulas and a unicorn. #MagicalDay`,
+    author: User.findOne({ firstName: 'Lord', lastName: 'Voldemort' })._id,
+    date: new Date('December 5, 2022, 3:12 PM EDT'),
+    replyCount: 12,
+    retweetCount: 622,
+    quoteTweetCount: 12,
+    likeCount: 1840,
+    bookmarkCount: 2,
+    createdOnSocialQ: false
+  }),
+  
+  new Tweet({
+    body: `Remember, I didn't choose the Dark Lord life. The Dark Lord life chose me.`,
+    author: User.findOne({ firstName: 'Lord', lastName: 'Voldemort' })._id,
+    date: new Date('January 21, 2023, 7:30 AM EDT'),
+    replyCount: 38,
+    retweetCount: 1065,
+    quoteTweetCount: 45,
+    likeCount: 4123,
+    bookmarkCount: 3,
+    createdOnSocialQ: false
+  }),
+  
+  new Tweet({
+    body: `Found this cool book on horcruxes. It's like a DIY guide to immortality. #LifeHacks`,
+    author: User.findOne({ firstName: 'Lord', lastName: 'Voldemort' })._id,
+    date: new Date('April 8, 2023, 11:58 AM EDT'),
+    replyCount: 27,
+    retweetCount: 732,
+    quoteTweetCount: 19,
+    likeCount: 2751,
+    bookmarkCount: 2,
+    createdOnSocialQ: false
+  }),
+  
+  new Tweet({
+    body: `When you're about to take over the wizarding world but your favorite show drops a new season on Netflix. Decisions, decisions. 📺🔮`,
+    author: User.findOne({ firstName: 'Lord', lastName: 'Voldemort' })._id,
+    date: new Date('May 30, 2023, 6:05 PM EDT'),
+    replyCount: 16,
+    retweetCount: 586,
+    quoteTweetCount: 28,
+    likeCount: 2154,
+    bookmarkCount: 1,
+    createdOnSocialQ: false
+  }),
 )
 
 for (let i = 0; i < NUM_SEED_TWEETS; i++) {
@@ -296,6 +501,31 @@ for (let i = 0; i < NUM_SEED_TWEETS; i++) {
     })
   )
 }
+
+//create categories
+const categories = []
+
+categories.push(
+  new Category({
+    name: 'spontaneous'
+  }),
+  new Category({
+    name: 'witty'
+  }),
+  new Category({
+    name: 'playful'
+  }),
+  new Category({
+    name: 'excited'
+  })
+)
+
+//create post categories
+const postCategories = []
+
+postCategories.push(
+  new PostCategory()
+)
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -317,6 +547,7 @@ mongoose
                    .then(() => Category.collection.drop())
                    .then(() => User.insertMany(users))
                    .then(() => Tweet.insertMany(tweets))
+                   .then(() => Category.insertMany(categories))
                    .then(() => {
                      console.log("Done!");
                      mongoose.disconnect();
