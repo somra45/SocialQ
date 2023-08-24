15c8a3e678b6b56d52b5c58721bb6c49e77e84ee
root <somra45@gmail.com>
2023-08-24 16:29:51 -0400

    fixed about page

--- frontend/src/components/AboutPage/index.js
+++ frontend/src/components/AboutPage/index.js
@@ -19,26 +19,30 @@ const AboutPage = () => {
             <br/>
             <p>About Me:</p>
             <br/>
-            <p>I love hiking, gaming, and traveling, pursuing my dream of  </p>
+            <p>I love hiking and gaming, and pursuing my dream of  </p>
             <p>traveling the world while I contribute to making it better</p>
             <br/>
-            < a href="https://github.com/somra45" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/theofanis-neofotistos-483b33254/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
+            < a href="https://github.com/somra45" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/harvinder-somra-39ba536a/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
             </div>
 
         </div>
 
         <div className='team-member-container'>
                 
-                <img className="team-member-picture"src='./assets/images/Max.png' alt='harvey'></img>
+                <img className="team-member-picture" src='./assets/images/Joe.png' alt='harvey'></img>
                 
                 <div className='team-member-bio'>
-                    <h1 className='team-member-name'>Max Carpenter</h1>
-                    <h2 className='team-member-role'>Full Stack Support Engineer</h2>
-                    <p>About Me:</p>
+                    <h1 className='team-member-name'>Joe Randazzo</h1>
+                    <h2 className='team-member-role'>Backend Engineer</h2>
+                    <br/>
+                    <p>About Me: </p>
+                    <br/>
+                    <p>When I'm not playing dodgeball, I'm equally passionate about academic</p>
+                    <p>pursuits and fostering meaningful connections with those around me</p>
                     <br/>
                     <p></p>
                     <br/>
-                    < a href="https://github.com/somra45" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/theofanis-neofotistos-483b33254/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
+                    < a href="https://github.com/jprandazzo" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/jprandazzo/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
                 </div>
 
         </div>
@@ -54,27 +58,24 @@ const AboutPage = () => {
                     <br/>
                     <p></p>
                     <br/>
-                    < a href="https://github.com/somra45" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/theofanis-neofotistos-483b33254/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
+                    < a href="https://github.com/TheofanisNeofotistos" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/theofanis-neofotistos-483b33254/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
                 </div>
 
         </div>
 
         <div className='team-member-container'>
                 
-                <img className="team-member-picture"src='./assets/images/Joe.png' alt='harvey'></img>
+                <img className="team-member-picture"src='./assets/images/Max.png' alt='harvey'></img>
                 
                 <div className='team-member-bio'>
-                    <h1 className='team-member-name'>Joe Randazzo</h1>
-                    <h2 className='team-member-role'>Backend Engineer</h2>
+                    <h1 className='team-member-name'>Max Carpenter</h1>
+                    <h2 className='team-member-role'>Full Stack Support Engineer</h2>
+                    <p>About Me:</p>
                     <br/>
-                    <p>About Me: </p>
+                    <p>Max is a software engineer, crossword constructor, film programmer,</p>
+                    <p> and writer based in New York.</p>
                     <br/>
-                    <p>When I'm not playing dodgeball, I'm equally passionate about academic</p>
-                    <p>pursuits and fostering meaningful connections with those around me</p>
-                    <br/>
-                    <p></p>
-                    <br/>
-                    < a href="https://github.com/somra45" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/theofanis-neofotistos-483b33254/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
+                    < a href="https://github.com/mxcrpntr" class="fa-brands fa-github fa-2xl" style={{color: "#ffff",}}> </a>  < a href="https://www.linkedin.com/in/max-carpenter-245746288/" class="fa-brands fa-linkedin fa-2xl" style={{color: "#ffff",}}> </a>
                 </div>
 
         </div>
